f4a439a0555ece2db89476232e992359077fe323
Keerthudarshu <Keerthudarshu06@gmail.com>
2025-08-29 17:19:29 +0530

    updated new changes 7th

---
+++ neenu-backend/controllers/productController.js
@@ -0,0 +1,8 @@
+const db = require('../config/db');
+
+exports.getAllProducts = (req, res) => {
+  db.query('SELECT * FROM products', (err, results) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json(results);
+  });
+};
